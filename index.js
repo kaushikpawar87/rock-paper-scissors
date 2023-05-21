e7f7d5155d6c42d74453b49ef6aafbae7f3a5b1e
kaushikpawar <kaushikpawar87@gmail.com>
2023-05-22 06:49:59 +1000

    Initial commit. Created getcomputerChoice function

---
+++ index.js
@@ -0,0 +1,16 @@
+
+function getComputerChoice() {
+    
+    let choice = Math.floor(Math.random() * 3)
+
+    if(choice === 0) {
+        return 'Rock'
+    }
+    else if(choice === 1) {
+        return 'Paper'
+    }
+    return 'Scissors'
+
+}
+
+console.log(getComputerChoice());
